70c66e5e9cfa39fc21b16828feb884b8fd0c49eb
dell <dell@DESKTOP-RP18PG8>
2023-03-23 20:57:59 +0530

    test

---
+++ SpringCoreSetterInjection/src/main/java/com/example/SpringCoreTestingForConformation/App.java
@@ -0,0 +1,33 @@
+package com.example.SpringCoreTestingForConformation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "using new keyword!" );
+        System.out.println("----------------------");
+        Employee emp=new Employee(101,"Virat","Mumbai");
+        System.out.println(emp);
+        
+        System.out.println("==========================");
+        
+        System.out.println("using Container !");
+        System.out.println("----------------------");
+       ApplicationContext context=new ClassPathXmlApplicationContext("context.xml");
+       Object emp1 = context.getBean("emp1");
+      
+       Object emp2 = context.getBean("emp2");
+      
+       Employee  emp3 = (Employee) context.getBean("emp3");
+        
+       System.out.println(emp1);
+       System.out.println(emp2);
+       System.out.println(emp3);
+    
+    }
+
+}
